7e666d6973bbb1b5ae20c4a27a0806d1a6eec225
KenanAkbarli <tu6m5m78u@code.edu.az>
2022-11-01 00:52:11 +0400

    feat: add Algorithm2 task with c#

---
+++ GetSumNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿
+/* Sum numbers*/
+
+/*
+Console.Write("Enter Number:");
+int GetNumber = int.Parse(Console.ReadLine());
+if(GetNumber < 0)
+{
+    GetNumber *= -1;
+}
+int sum = 0;
+int i;
+for(i = 1; i <= GetNumber; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine("Answer:" + sum);
+*/
+
+int i;
+for (i = 1; i < 100; i++)
+{
+  if (i % 3 == 0 || i % 5 == 0)
+    {
+
+    Console.WriteLine(i);
+    }
+}
